c6b9a020c71f3ff6d35978f62bc0c970c7ab2282
Shukshina <Shukshina.MI@parmalogica.ru>
2019-06-19 10:24:08 +0500

    add unobservable

--- src/app/mymeny/input/input.component.ts
+++ src/app/mymeny/input/input.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {DataService} from '../../data.service';
 import {fromEvent, Observable} from 'rxjs';
 import {mapTo} from 'rxjs/operators';
@@ -8,29 +8,33 @@ import {mapTo} from 'rxjs/operators';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit, AfterViewInit {
+export class InputComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @ViewChild('inputEl', ({static: false})) inputEl: ElementRef;
   public inputData: string;
+  public oservableSubscribe: any;
 
   constructor(private dataService: DataService) {
   }
 
   ngAfterViewInit(): void {
     const observable$: Observable<any> = fromEvent(this.inputEl.nativeElement, 'input');
-    observable$.subscribe((res: any) => {
+    this. oservableSubscribe = observable$.subscribe((res: any) => {
       this.inputData = res.target.value;
     });
     console.log('update input');
   }
 
-  enterData() {
+  enterData(): void {
     this.dataService.setInputData(this.inputData);
   }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(): void {
+    this.oservableSubscribe.unsubscribe();
+  }
 
 
 }
--- src/app/output2/output2.component.ts
+++ src/app/output2/output2.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {DataService} from '../data.service';
+import {Subscriber} from 'rxjs';
 
 @Component({
   selector: 'app-output2',
   templateUrl: './output2.component.html',
   styleUrls: ['./output2.component.css']
 })
-export class Output2Component implements OnInit {
+export class Output2Component implements OnInit, OnDestroy {
 
   public inputData: string;
+  public inputDataSubscribe: any;
 
   constructor(private dataService: DataService) {
   }
 
   ngOnInit() {
-    this.dataService.getInputData().subscribe((data: string) => {
+    this.inputDataSubscribe = this.dataService.getInputData().subscribe((data: string) => {
       console.log('output2 ', data);
       this.inputData = data;
     });
   }
 
+  ngOnDestroy() {
+    this.inputDataSubscribe.unsubscribe();
+  }
 }
